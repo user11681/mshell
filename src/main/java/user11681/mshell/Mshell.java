2c50d431ad390cad5e61ed58afff13dd452cf9c1
user11681 <username11681@gmail.com>
2020-07-14 15:17:23 +0100

    initial commit

---
+++ src/main/java/user11681/mshell/Mshell.java
@@ -0,0 +1,10 @@
+package user11681.mshell;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Mshell implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/user11681/mshell/client/MShellClient.java
@@ -0,0 +1,13 @@
+package user11681.mshell.client.client;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.Environment;
+import net.fabricmc.api.EnvType;
+
+@Environment(EnvType.CLIENT)
+public class MShellClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
